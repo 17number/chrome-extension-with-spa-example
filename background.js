2526cf15419c38f999e2314c9da496ea03f5e5cf
r17n <4716989+17number@users.noreply.github.com>
2019-10-27 11:32:19 +0900

    research chrome.tabs.executeScript

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  console.log({
-    tabId: tabId,
-    changeInfo: changeInfo,
-    tab: tab,
-    tabUrl: tab.url,
-    locationHref: location.href,
-    this: this
-  });
-  if (changeInfo.status === "complete" && tab.url.indexOf("https://www.google.com/") > -1) {
+  if (changeInfo.status === "complete" && tab.url.indexOf("https://www.google.com/search") > -1) {
     console.log(`updated: ${tab.url}`);
+    chrome.tabs.executeScript(
+      tabId,
+      {
+        file: "hello.js",
+      },
+      function(result) {
+        console.log(`executed: ${result}`);
+      }
+    );
   }
 });
